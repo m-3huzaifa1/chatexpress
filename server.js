6b06bdf699d22db737c0f4667efc1b7c6f61fc48
Mohd Huzaifa <mohdhuzaifa80@gmail.com>
2022-11-01 13:50:38 +0530

    ChatExpress

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ start();
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-if (process.env.NODE_ENV === 'production')
+if (process.env.NODE_ENV == 'production')
 {
   app.use('/',express.static('client/build'))
   app.get("*",(req,res)=>{
